d583516b1df5191e5e23b70fa216caec723b8e6c
camilotavera1 <camilo.tavera@cun.edu.co>
2016-09-19 07:28:18 -0500

    animal view

--- public/js/animal.js
+++ public/js/animal.js
@@ -1,14 +1,25 @@
 var Age_on = 6;
-
-//function click_button(){
-  //192.168.0.6
-  //setTimeout(httpGet("http://192.168.0.6"),500);
-//  setTimeout(turnOn,500);
-//  setTimeout(turnOff,time_on);
-
+var Peso_on = 600;
+var Porciones_on = 3;
+var Grams_on = 200;
 
   function updateAgeLabel(Age) {
     console.log(Age);
     document.getElementById("Age_Value_label").innerHTML = Age;
     Age_on = Age;
+  }
+  function updatePesoLabel(Peso) {
+    console.log(Peso);
+    document.getElementById("Peso_Value_label").innerHTML = Peso;
+    Peso_on = Peso;
+  }
+  function updatePorcionesLabel(Porciones) {
+    console.log(Porciones);
+    document.getElementById("Porciones_Value_label").innerHTML = Porciones;
+    Porciones_on = Porciones;
+  }
+  function updateGramsLabel(Grams) {
+    console.log(Grams);
+    document.getElementById("Grams_Value_label").innerHTML = Grams;
+    Grams_on = Grams;
   }
